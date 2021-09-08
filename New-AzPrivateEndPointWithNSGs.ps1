0197a81a0af8c28fd6f8dc56d5a6309b7db0a3f8
Scott Metzel <scottmetzel@microsoft.com>
2021-09-07 20:01:10 -0700

    * Tiny modification to control preview feature registration

--- New-AzPrivateEndPointWithNSGs.ps1
+++ New-AzPrivateEndPointWithNSGs.ps1
@@ -10,44 +10,46 @@
 param (
     [System.String]$VirtualNetworkName = "Dev-VNET-Hub-01",
     [System.String]$VirtualNetworkSubnetName = "PL-KeyVault01",
-    [System.String]$KeyVaultName = "Dev-KV-ADDS-01"
+    [System.String]$KeyVaultName = "Dev-KV-ADDS-01",
+    [System.Boolean]$RegisterPreviewNSGFeature = $false
 )
 ## Get information stream messages to show, and make sure we stop on error
 $InformationPreference = "Continue"
 $ErrorActionPreference = "Stop"
 
 ## Get the Preview Feature state
-$GetAzProviderFeature = Get-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
-[System.String]$AzProviderFeatureRegistrationState = $GetAzProviderFeature.RegistrationState
+if ($true -eq $RegisterPreviewNSGFeature) {
+    $GetAzProviderFeature = Get-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
+    [System.String]$AzProviderFeatureRegistrationState = $GetAzProviderFeature.RegistrationState
 
-## Register for it if not registered
-if ("NotRegistered" -eq $AzProviderFeatureRegistrationState) {
-    Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Registering."
-    Register-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
-}
-elseif ("Pending" -eq $AzProviderFeatureRegistrationState) {
-    Write-Warning -Message "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Please try again in a bit."
-    throw
-}
-else {
-    Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Skipping registration."
-}
-
-######## Loop until the preview feature is registered
-do {
-    Write-Information -MessageData "Checking preview feature registration state again..."
-    $GetAzProviderFeatureAgain = Get-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
-    [System.String]$AzProviderFeatureRegistrationStateAgain = $GetAzProviderFeatureAgain.RegistrationState
-    Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'"
-
-    if ("Registered" -ne $AzProviderFeatureRegistrationStateAgain) {
-        Write-Information -MessageData "Preview Feature registration state is not 'Registered'. Waiting for 5 seconds..."
-        Start-Sleep -Seconds 5
+    ## Register for it if not registered
+    if ("NotRegistered" -eq $AzProviderFeatureRegistrationState) {
+        Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Registering."
+        Register-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
+    }
+    elseif ("Pending" -eq $AzProviderFeatureRegistrationState) {
+        Write-Warning -Message "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Please try again in a bit."
+        throw
+    }
+    else {
+        Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'. Skipping registration."
     }
 
-} while ("Registered" -ne $AzProviderFeatureRegistrationStateAgain)
-######## End Loop
+    ######## Loop until the preview feature is registered
+    do {
+        Write-Information -MessageData "Checking preview feature registration state again..."
+        $GetAzProviderFeatureAgain = Get-AzProviderFeature -ProviderNamespace "Microsoft.Network" -FeatureName "AllowPrivateEndpointNSG"
+        [System.String]$AzProviderFeatureRegistrationStateAgain = $GetAzProviderFeatureAgain.RegistrationState
+        Write-Information -MessageData "Preview Feature state is: '$AzProviderFeatureRegistrationState'"
 
+        if ("Registered" -ne $AzProviderFeatureRegistrationStateAgain) {
+            Write-Information -MessageData "Preview Feature registration state is not 'Registered'. Waiting for 5 seconds..."
+            Start-Sleep -Seconds 5
+        }
+
+    } while ("Registered" -ne $AzProviderFeatureRegistrationStateAgain)
+    ######## End Loop
+}
 ## Parameter splat for the get virtual network cmdlet
 [System.Collections.Hashtable]$GetAzVNETSplat = @{
     Name = $VirtualNetworkName
