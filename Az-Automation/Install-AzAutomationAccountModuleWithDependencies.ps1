47b529e2dab6c501f7bae6d8a40cd8b0a9b4e883
Scott Metzel <scottmetzel@microsoft.com>
2021-09-15 12:17:11 -0700

    * Added basic param block
    * Changed hashtable to ArrayList and added dependencies to it
    
    * Dependencies now install before parent module
    
    * Dependency provisioning state works
    
    * Parent module installation just might work - haven't tested

--- Az-Automation/Install-AzAutomationAccountModuleWithDependencies.ps1
+++ Az-Automation/Install-AzAutomationAccountModuleWithDependencies.ps1
@@ -1,6 +1,10 @@
+param (
+    [System.String]$AutomationAccountResourceGroupName,
+    [System.String]$AutomationAccountName,
+    [System.String[]]$Modules = @("Az")
+)
 $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
-[System.Collections.ArrayList]$Modules = @("Az")
 
 Write-Information -MessageData "Finding modules..."
 [System.Int32]$c = 1
@@ -10,8 +14,14 @@ foreach ($Module in $Modules) {
 
     $FindModule = Find-Module -Name $Module
 
-    [System.Collections.Hashtable]$ModuleDependencies = $FindModule.Dependencies
 
+    [System.Collections.ArrayList]$ModuleDependencies = @()
+
+    $FindModule.Dependencies | ForEach-Object -Process {
+        $ModuleDependencies.Add($_) | Out-Null
+    }
+
+    Write-Information -MessageData "Finding if module has dependencies."
     if ($ModuleDependencies.Count -gt 0) {
         Write-Warning -Message "Module has dependencies. Getting those first."
 
@@ -19,34 +29,53 @@ foreach ($Module in $Modules) {
         [System.Int32]$ModuleDependencyCount = $ModuleDependencies.Count
         foreach ($Dependency in $ModuleDependencies) {
             [System.String]$ModuleDependencyName = $Dependency.Name
-            [System.String]$ModuleDependencySourceLocation = $Dependency.RepositorySourceLocation
-            [System.String]$ModuleDependencyVersion = $Dependency.Version
-            [System.String]$ModuleURI = [System.String]::Concat($ModuleDependencySourceLocation, "/package/", $ModuleDependencyName, "/", $ModuleDependencyVersion)
+            Write-Information -MessageData "Finding dependent module: '$ModuleDependencyName'."
+            $FindDependendModule = Find-Module -Name $ModuleDependencyName
+
+            [System.String]$ModuleDependencySourceLocation = $FindDependendModule.RepositorySourceLocation
+            [System.String]$ModuleDependencyVersion = $FindDependendModule.Version
+            [System.String]$ModuleDependencyURI = [System.String]::Concat($ModuleDependencySourceLocation, "/package/", $ModuleDependencyName, "/", $ModuleDependencyVersion)
             Write-Information -MessageData "Working on module dependency: '$ModuleDependencyName'. Dependency: '$i' of: '$ModuleDependencyCount'."
 
             try {
                 $ErrorActionPreference = "Stop"
-                New-AzAutomationModule -ResourceGroupName $AutomationAccountResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ModuleDependencyName -ContentLinkUri $ModuleURI
+                New-AzAutomationModule -ResourceGroupName $AutomationAccountResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ModuleDependencyName -ContentLinkUri $ModuleDependencyURI
+
+                Write-Information -MessageData "Checking module dependency installation state."
+                do {
+                    $GetAzAutomationModuleDependencyInAccount = Get-AzAutomationModule -ResourceGroupName $AutomationAccountResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ModuleDependencyName -ErrorAction SilentlyContinue
+
+                    [System.String]$ModuleDependencyProvisioningState = $GetAzAutomationModuleDependencyInAccount.ProvisioningState
+
+                    [System.Boolean]$ModuleDependencyInstalled = $false
+                    if ($ModuleDependencyProvisioningState -notin @("Created", "Succeeded")) {
+                        Write-Information -MessageData "Module dependency: '$ModuleDependencyName' is not installed. State: '$ModuleDependencyProvisioningState'. Rechecking in 5 seconds."
+                        [System.Boolean]$ModuleDependencyInstalled = $false
+                        Start-Sleep -Seconds 5
+                    }
+                    else {
+                        Write-Information -MessageData "Module dependency: '$ModuleDependencyName' is installed. State: '$ModuleDependencyProvisioningState'. Moving to next module dependency."
+
+                        [System.Boolean]$ModuleDependencyInstalled = $true
+                    }
+
+                } while ($false -eq $ModuleDependencyInstalled)
             }
             catch {
                 $_
                 throw
             }
+
+            $i++
         }
     }
+
+    # Now install the module
+    [System.String]$ModuleName = $Dependency.Name
+    [System.String]$ModuleSourceLocation = $Dependency.RepositorySourceLocation
+    [System.String]$ModuleVersion = $Dependency.Version
+    [System.String]$ModuleURI = [System.String]::Concat($ModuleSourceLocation, "/package/", $ModuleName, "/", $ModuleVersion)
+    Write-Information -MessageData "Installing module: '$Module'. Module: '$c' of: '$ModuleCount'."
+    New-AzAutomationModule -ResourceGroupName $AutomationAccountResourceGroupName -AutomationAccountName $AutomationAccountName -Name $ModuleName -ContentLinkUri $ModuleURI
     $c++
-}
-
-#necessary modules list
-$deps1 = @("Az.Accounts", "Az.Storage", "Az.Compute")
-
-foreach ($dep in $deps1) {
-    $module = Find-Module -Name $dep
-    $link = $module.RepositorySourceLocation + "/package/" + $module.Name + "/" + $module.Version
-    New-AzAutomationModule -AutomationAccountName $AutomationAccountName -Name $module.Name -ContentLinkUri $link -ResourceGroupName $ResourceGroupName
-    if ($dep -eq "Az.Accounts") {
-        #Az.Accounts is a dependency for Az.Storage and Az.Compute modules
-        Write-Host "Sleeping for 180 sec in order to wait the installation of the Az.Accounts module"
-        Start-Sleep 180
-    }
 }
